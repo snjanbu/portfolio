61ff7d6a545256599791f1d0ed2cfafeda8100d6
Sanjay Anbu <snjanbu@gmail.com>
2025-01-04 21:20:57 +0530

    initial files added

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+
+      function isNavPill(element) {
+        return $(element).attr('data-toggle') && $(element).attr('data-toggle') == 'pill';
+      }
+
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        if (!isNavPill($anchor)) {
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+          event.preventDefault();
+        }
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
